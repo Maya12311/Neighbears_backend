11afa8b8804c25e1b2b75167c2e6e276fd12ca63
Mariana <marianaschmal@yahoo.de>
2025-08-29 16:03:32 +0200

    added the methods in controller and service class and repository to get the current user profile pic

--- src/main/java/com/example/neighbears/repository/ImageRepository.java
+++ src/main/java/com/example/neighbears/repository/ImageRepository.java
@@ -3,6 +3,8 @@ package com.example.neighbears.repository;
 import com.example.neighbears.model.Image;
 import org.springframework.data.repository.CrudRepository;
 
-public interface ImageRepository extends CrudRepository<Image, Long> {
+import java.util.Optional;
 
+public interface ImageRepository extends CrudRepository<Image, Long> {
+Optional<Image> findFirstByCustomerIdOrderByUploadedAtDesc(Long customerId);
 }
